baae0d4710ccb0dee62c32f56826e4b2db21b235
Kevin Mulot <kevin.mulot@gmail.com>
2019-06-18 11:59:53 +0200

    Tracy debugger enabled

--- public/index.php
+++ public/index.php
@@ -1,7 +1,12 @@
 <?php
 
 // Loads Composer autoload
-require_once dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+// Use library debugger
+use Tracy\Debugger;
+
+Debugger::enable();
 
 
 use Controller\FrontController;
