00fb7e71d00446b045ac4acd1e87ea256d789b21
Kevin Mulot <kevin.mulot@gmail.com>
2019-06-18 12:32:31 +0200

    Minor update

--- src/Model/Manager.php
+++ src/Model/Manager.php
@@ -1,26 +1,40 @@
 <?php
+
 namespace Model;
+
 use Exception;
 use PDO;
 
-require_once dirname(__DIR__) .'./config/config.php';
+
+/**
+ * Class Manager
+ * @package Model
+ */
 abstract class Manager
 {
-    protected $db;
+    /**
+     * @var null
+     */
+    static protected $db = null;
+
     /**
      * @return PDO
      * Function for database connection which uses data in config/config
      */
     public function connectDB()
     {
-        try {
+        require_once dirname(__DIR__) . './config/config.php';
+
+        if (is_null(self::$db)) {
+
             $db = new PDO('mysql:host=' . HOST_NAME . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PWD);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-            return $this->db;
-        } catch (Exception $e) {
-            die('Erreur : ' . $e->getMessage());
+
+            self::$db = $db;
+
+            return $db;
         }
+        return self::$db;
     }
 }
 
