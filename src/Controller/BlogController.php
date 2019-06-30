2b16f588789509a8ee2470fcff10285b0d1fba1a
Kevin Mulot <kevin.mulot@gmail.com>
2019-06-30 14:13:58 +0200

    Secured the GET method

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,7 +3,6 @@
 namespace Controller;
 
 use Model\CommentManager;
-use Model\Post;
 use Model\PostManager;
 
 session_start();
@@ -29,7 +28,7 @@ class BlogController extends Controller
      */
     public function readAction()
     {
-        $id = $_GET['id'];
+        $id = filter_input(INPUT_GET, 'id',FILTER_SANITIZE_NUMBER_INT);
         $post = (new PostManager)->getPost($id);
         $comments = (new CommentManager)->getComments($id);
 
