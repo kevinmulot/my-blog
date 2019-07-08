822504af56580fc4e14dcf0eabfddacf897c7f41
Kevin Mulot <kevin.mulot@gmail.com>
2019-07-08 17:39:11 +0200

    Session starts when you run the app

--- public/index.php
+++ public/index.php
@@ -6,6 +6,10 @@ use Tracy\Debugger;
 // Loads Composer autoload
 require_once  '../vendor/autoload.php';
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 Debugger::enable();
 
 $frontController = new FrontController();
